7ee936b68bb6e6444c4f6d05c325947da045d9a9
jasonmcgill <64054045+jasonmcgill@users.noreply.github.com>
2020-06-27 22:18:53 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,130 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
+// array of questions for user
+const questions = [{
+        type: 'input',
+        name: 'name',
+        message: 'What is the project title? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Enter a description of the project: (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter a description');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some installation instructions or examples of usages?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide the installation instructions/examples of usages:',
+        when: ({
+            confirmAbout
+        }) => {
+            return confirmAbout
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContributors',
+        message: 'Would you like to enter any contributors?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Please provide contributors github names:',
+        when: ({
+            confirmContributors
+        }) => {
+            return confirmContributors
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmGuidelines',
+        message: 'Would you like to enter any contribution guidelines?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'guidelines',
+        message: 'Please enter any contribution guidelines:',
+        when: ({
+            confirmGuidelines
+        }) => {
+            return confirmGuidelines
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTest',
+        message: 'Would you like to enter any test instructions?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter any test instructions:',
+        when: ({
+            confirmTest
+        }) => {
+            return confirmTest
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What license does your provide?',
+        choices: ['Apache', 'GNU', 'MIT']
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your GitHub username: (Required)',
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub Username!')
+                return false;
+            }
+        }  
+    }
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
-}
+    console.log("Hi! Lets generate a readme!")
+    inquirer.prompt(questions)
+        .then(generateMarkdown)
+        .catch(error => {
+            console.log("Something went wrong " + error.message);
+        })
+};
 
 // function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+
+## Badges
+
+## Description 
+
+
+## Table of Contents
+
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license)
+* [Guidelines](#guidelines)
+* [Tests](#tests)
+* [Questions?](#questions?)
+
+
+
+## Installation Instructions
+
+## Usage 
+
+## Contributors
+
+## License
+---
+## Contribution guidelines
+
+## Tests
+
+## Questions? 
+Send me a message on GitHub or E-mail me at: 
 
 `;
 }
